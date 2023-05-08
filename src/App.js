9e993442fe3ef402bc954298e677d453b236b007
ssinlao <114186529+ssinlao@users.noreply.github.com>
2023-05-07 23:10:58 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./component/List";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <List />
+      </header>
+    </div>
+  );
+}
+
+export default App;
